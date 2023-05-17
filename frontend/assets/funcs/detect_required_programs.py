b7ac910f2feef38e2419ac79bece7420414d1bf6
Parker <pp101socials@gmail.com>
2023-05-17 12:12:24 -0400

    Moved sample_code contents to file

--- frontend/assets/funcs/detect_required_programs.py
+++ frontend/assets/funcs/detect_required_programs.py
@@ -2,6 +2,7 @@
 import pyodbc
 import os
 
+# Contents from sample_code
 def find_ssms_installation_location():
     '''
     This is used to detect if the user has SSMS installed on their computer
@@ -15,12 +16,12 @@ def find_ssms_installation_location():
         # Add more possible locations if applicable
     ]
 
+    # Checking all of the locations at the top to see if that path exists
     for location in possible_locations:
         if os.path.exists(location):
             return location
 
     return None  # SSMS not found
-
 def locating_odbc_drivers():
     '''
     This is used to check if ODBC Drivers are installed
@@ -30,10 +31,10 @@ def locating_odbc_drivers():
     driver_name = "ODBC Driver 17 for SQL Server"
     drivers = [driver for driver in pyodbc.drivers() if driver == driver_name] # Goes through all the drivers to find one that matches the driver name
     return bool(drivers)
-
 detect_odbc_drivers = locating_odbc_drivers()
 ssms_installation_location = find_ssms_installation_location()
 
+# Original content
 def detect_required_programs():
     '''
     This will check if both programs are installed and will return its respected value
@@ -79,5 +80,4 @@ def detect_required_programs():
         bothFound = "Both programs were found"
 
         return bothFound
-
 check_for_required_programs = detect_required_programs()
